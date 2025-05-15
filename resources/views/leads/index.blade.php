4696b060aacd16d5431ec71af1d9a0a5196ad2fd
Cotocea <denis.nechifor@student.unitbv.ro>
2025-05-15 07:19:31 +0300

    Scraping OLX

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-8xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class=" dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="mb-2 mt-2 me-2 text-end">
                     <x-link-primary-button href="{{ route('leads.create') }}">
