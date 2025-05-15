6a13b32d4ba5270d3c59b4a29c7a215325280f33
Cotocea <denis.nechifor@student.unitbv.ro>
2025-05-15 07:24:23 +0300

    Scraping OLX

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,8 +1,9 @@
 @props(['name', 'label' => null])
 
 <div class="mb-4 mt-4 flex items-center">
-    <input type="checkbox" name="{{ $name }}" id="{{ $name }}"
-        {{ old($name) ? 'checked' : '' }}
+    <input type="hidden" name="{{ $name }}" value="0">
+    <input type="checkbox" name="{{ $name }}" id="{{ $name }}" value="1"
+        {{ old($name) == 1 ? 'checked' : '' }}
         {{ $attributes->merge(['class' => 'h-4 w-4 text-indigo-500 focus:ring-indigo-500 dark:bg-gray-900 dark:border-gray-700']) }}>
     <label for="{{ $name }}" class="ml-2 text-sm text-gray-700 dark:text-gray-300">{{ $label }}</label>
 
